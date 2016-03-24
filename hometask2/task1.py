cb867c6f617bb9aae349e4d3a9db2e8b28e67a56
Kiryl Filatau <kiryl_filatau@epam.com>
2016-03-24 18:45:49 +0300

    task1

--- hometask2/task1.py
+++ hometask2/task1.py
@@ -1,4 +1,6 @@
-#!/bin/python
-list1 = [1,2,3,4,5]
-list2 = ['a','b','c','d']
-
+#!/usr/bin/python
+kflist1 = [1,2,3,4,5,6,7]
+kflist2 = ['a','b','c','d']
+def d1(l1, l2):
+    return  dict(zip(l1,l2 +[None]*(len(l1)-len(l2))))
+print(d1(kflist1,kflist2))
