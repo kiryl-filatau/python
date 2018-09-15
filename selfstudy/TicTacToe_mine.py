13b90f3fa053737b7520c5be818ad852e76d3afd
Kiryl Filatau <kiryl_filatau@epam.com>
2018-09-15 18:46:32 +0300

    add retry

--- selfstudy/TicTacToe_mine.py
+++ selfstudy/TicTacToe_mine.py
@@ -20,18 +20,16 @@ def display_board(board):
     print('         -----------')
     print('          ' + board[0]+ ' | ' +board[1]+ ' | ' +board[2] + ' ')
 #random choice who will start
+
 def who_is_first(player1,player2):
     os.system('clear')
-    rules_func()
-    info_board_func(info_board)
-    player1 = input('could you please enter your name, one of you:  ')
-    player2 = input('another one: ')
     order = [player1,player2]
     player1 = random.choice(order)
     order.remove(player1)
     player2 = order[0]
     print(f'{player1} will play first and use "X" and {player2} will play second and use "O"')
-    return(player1,player2)
+    info_board_func(info_board)
+    return[player1,player2]
 
 #to chose number for proper cell
 def player_number(player):
@@ -79,7 +77,9 @@ def player_2(start_board,player2):
             display_board(start_board)
             return False
     else:
+        os.system('clear')
         print('Value is out of range 1 to 9')
+        display_board(start_board)
         return False
     return start_board
 #### skip if no winner is possible ####
@@ -95,22 +95,33 @@ def win(start_board,winner,player1,player2):
         # print(win_lines)
         if len(set(win_lines)) == 1 and win_lines[0] == 'X':
             print(f'The winner is {player1}')
-            winner = False
-            exit()
         elif len(set(win_lines)) == 1 and win_lines[0] == 'O':
             print(f'The winner is {player2}')
-            winner = False
-            exit()
+    return False
     for sets in winner_board:
         if ' ' not in start_board:
             print('The winner is friendship')
-            winner = False
+    return False
+
+def retry():
+    global start_board
+    start_board = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
+    check_choise = True
+    print(check_choise)
+    while True:
+        choice = input('would you like to play once again, print "yes" or "no": ')
+        clear_output()
+        os.system('clear')
+        if choice.lower() == 'yes':
+            return False
+        elif choice.lower() == 'no':
             exit()
+        else:
+            print('you should choose only between "yes" or "no"')
 
 #### VARIABLES ####
-player1 = ''
-player2 = ''
-check_choise = True
+retry_game = True
+new_game = True
 first_player_step = True
 winner = True
 start_board = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
@@ -123,20 +134,24 @@ winner_board = (
 )
 
 #### LOGIC ####
-while check_choise:
+rules_func()
+info_board_func(info_board)
+player1 = input('could you please enter your name, one of you:  ')
+player2 = input('another one: ')
+while new_game:
     players = who_is_first(player1,player2)
-    if True:
-        check_choise = False
-        break
+    new_game = False
+    break
 
-while winner:
+while not new_game:
     while first_player_step:
         if player_1(start_board,players[0]):
             first_player_step = False
-            win(start_board,winner,players[0],players[1])
         break
     while not first_player_step:
         if player_2(start_board,players[1]):
             first_player_step = True
-            win(start_board,winner,players[0],players[1])
         break
+    if not win(start_board,winner,players[0],players[1]):
+        new_game = True
+        retry()
