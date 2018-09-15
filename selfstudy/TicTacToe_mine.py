4d54c89cc2823d547a4d9691a7ae382e97678138
Kiryl Filatau <kiryl_filatau@epam.com>
2018-09-15 15:43:20 +0300

    added random choice and saved unused func

--- selfstudy/TicTacToe_mine.py
+++ selfstudy/TicTacToe_mine.py
@@ -1,6 +1,5 @@
 from IPython.display import clear_output
-import os
-
+import os, random
 #### FUNCTIONS ####
 #just for printing rules when game starts
 def rules_func():
@@ -20,57 +19,64 @@ def display_board(board):
     print('          ' + board[3]+ ' | ' +board[4]+ ' | ' +board[5] + ' ')
     print('         -----------')
     print('          ' + board[0]+ ' | ' +board[1]+ ' | ' +board[2] + ' ')
-#to choose X or O 
-def player_choice():
-    choice = input('select X or O: ')
-    clear_output()
+#random choice who will start
+def who_is_first(player1,player2):
     os.system('clear')
-    if choice.lower() == 'x':
-        print(f'you choice is {choice.upper()} you will play as Player 1')
-        info_board_func(info_board)
-    elif choice.lower() == 'o':
-        print(f'you choice is {choice.upper()} you will play as Player 2')
-        info_board_func(info_board)
-    else:
-        print("you should choose only between X and O")
-    return choice
+    rules_func()
+    info_board_func(info_board)
+    player1 = input('could you please enter your name, one of you:  ')
+    player2 = input('another one: ')
+    order = [player1,player2]
+    player1 = random.choice(order)
+    order.remove(player1)
+    player2 = order[0]
+    print(f'{player1} will play first and use "X" and {player2} will play second and use "O"')
+    return(player1,player2)
+
 #to chose number for proper cell
-def player_number(order):
+def player_number(player):
     while True:
         try:
-            step = int(input(f'Player {order} could you please enter number from 1 to 9: '))
+            step = int(input(f'{player} could you please enter number from 1 to 9: '))
             if step in range(1,10):
-                clear_output()
                 os.system('clear')
                 return step
             else:
                 return ValueError
         except ValueError:
+            os.system('clear')
             print('The value you entered is not a number')
+            display_board(start_board)
             continue
         else:
             break
 #player 1 step 
-def player_1(start_board):
-    step = player_number('1')
+def player_1(start_board,player1):
+    step = player_number(player1)
     if step in range(1,10):
         if start_board[step-1] == ' ':
             start_board[step-1] = 'X'
+            display_board(start_board)
         else:
+            os.system('clear')
             print('This cell was already used')
+            display_board(start_board)
             return False
     else:
         print('Value is out of range 1 to 9')
         return False
     return start_board
 #player 2 step
-def player_2(start_board):
-    step = player_number('2')
+def player_2(start_board,player2):
+    step = player_number(player2)
     if step in range(1,10):
         if start_board[step-1] == ' ':
             start_board[step-1] = 'O'
+            display_board(start_board)
         else:
+            os.system('clear')
             print('This cell was already used')
+            display_board(start_board)
             return False
     else:
         print('Value is out of range 1 to 9')
@@ -78,7 +84,7 @@ def player_2(start_board):
     return start_board
 #### skip if no winner is possible ####
 #define that someone wins
-def win(start_board,winner):
+def win(start_board,winner,player1,player2):
     check_board = [
         [start_board[0],start_board[1],start_board[2]],
         [start_board[3],start_board[4],start_board[5]],
@@ -88,11 +94,11 @@ def win(start_board,winner):
         win_lines = [check_board[x][y] for (x,y) in sets]
         # print(win_lines)
         if len(set(win_lines)) == 1 and win_lines[0] == 'X':
-            print('The winner is Player 1')
+            print(f'The winner is {player1}')
             winner = False
             exit()
         elif len(set(win_lines)) == 1 and win_lines[0] == 'O':
-            print('The winner is Player 2')
+            print(f'The winner is {player2}')
             winner = False
             exit()
     for sets in winner_board:
@@ -102,7 +108,8 @@ def win(start_board,winner):
             exit()
 
 #### VARIABLES ####
-
+player1 = ''
+player2 = ''
 check_choise = True
 first_player_step = True
 winner = True
@@ -116,25 +123,20 @@ winner_board = (
 )
 
 #### LOGIC ####
-rules_func()
-info_board_func(info_board)
-
 while check_choise:
-    player_input = player_choice()
-    if player_input.lower() == 'x' or player_input.lower() == 'o':
+    players = who_is_first(player1,player2)
+    if True:
         check_choise = False
         break
 
 while winner:
     while first_player_step:
-        if player_1(start_board):
-            display_board(start_board)
+        if player_1(start_board,players[0]):
             first_player_step = False
-            win(start_board,winner)
+            win(start_board,winner,players[0],players[1])
         break
     while not first_player_step:
-        if player_2(start_board):
-            display_board(start_board)
+        if player_2(start_board,players[1]):
             first_player_step = True
-            win(start_board,winner)
+            win(start_board,winner,players[0],players[1])
         break
---
+++ selfstudy/func_not_in_use.py
@@ -0,0 +1,14 @@
+#to choose X or O 
+# def player_choice():
+#     choice = input('select X or O: ')
+#     clear_output()
+#     os.system('clear')
+#     if choice.lower() == 'x':
+#         print(f'you choice is {choice.upper()} you will play as Player 1')
+#         info_board_func(info_board)
+#     elif choice.lower() == 'o':
+#         print(f'you choice is {choice.upper()} you will play as Player 2')
+#         info_board_func(info_board)
+#     else:
+#         print("you should choose only between X and O")
+#     return choice
